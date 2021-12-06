92f0e0432eb7733848957b05b350b822bab2fc16
richcastro82 <rich.castro@achesongroup.com>
2021-12-05 22:12:37 -0500

    Weekend coding

--- rules.py
+++ rules.py
@@ -4,14 +4,22 @@
 
 import pygame,sys
 from vars import *
-#
+
 
 class Button:
     def __init(self, x, y, w, h, text=""):
-        pass
+        self.x=x
+        self.y=y
+        self.width=w
+        self.height=h
+        self.text=text
 
     def DrawButton(self):
         pass
+
+    def isOver(self, pos):
+        pass
+
 
 class Board:
     def __init__(self, width, height, lines, size):
@@ -21,17 +29,21 @@ class Board:
         self.size=size
 
     def StartMenu(self):
-        # pass
         bg=pygame.image.load('bg.png')
+        START_BUT=Button(10,10,40,80,"Start Game")
+        QUIT_BUT=Button(20,20,40,80,"Quit Game")
+        START_BUT.DrawButton()
+        QUIT_BUT.DrawButton()
         run_game=False
         while run_game==False:
-
             screen.blit(bg, (0,0))
-            # clock.tick(fps)
             pygame.display.update()
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     run_game=True
+
+    def PauseScreen(self):
+        pass
 
     def QuitGame(self):
         pygame.quit()
@@ -42,46 +54,3 @@ class Board:
             for y in range(0,self.height,self.size):
                 pygame.draw.line(screen, reddish, [x,y],[x, y+self.width],2)
                 pygame.draw.line(screen, reddish, [x, y], [x+self.height, y], 2)
-
-
-
-# def drawGrid(width, height, lines, gridBox):
-#     for x in range(height):
-#         for y in range(width):
-#             rect=pygame.Rect(x*(gridBox+lines), y*(gridBox+lines), gridBox, gridBox)
-#             pygame.draw.rect(screen, lines, rect)
-
-#
-# def home_screen():
-#     pass
-
-# def game_over():
-#     pygame.quit()
-#     sys.exit()
-# #
-# def drawLine():
-#     for x in range (0, 600, 66 ):
-#         for y in range(0,600,66):
-#             pygame.draw.line(screen, reddish, [x,y],[x,y+width], 4)
-#             pygame.draw.line(screen, reddish, [x,y],[x+height,y], 4)
-    # Need to loop this code
-    # pygame.draw.line(screen, reddish, [x, y], [x,y+600], 4)
-    # pygame.draw.line(screen, reddish, [x+gridBox, y], [x+gridBox,y+600], 4)
-    # pygame.draw.line(screen, reddish, [x+gridBox*2, y], [x+gridBox*2,y+600], 4)
-    # pygame.draw.line(screen, reddish, [x+gridBox*3, y], [x+gridBox*3,y+600], 4)
-    # pygame.draw.line(screen, reddish, [x+gridBox*4, y], [x+gridBox*4,y+600], 4)
-    # pygame.draw.line(screen, reddish, [x+gridBox*5, y], [x+gridBox*5,y+600], 4)
-    # pygame.draw.line(screen, reddish, [x+gridBox*6, y], [x+gridBox*6,y+600], 4)
-    # pygame.draw.line(screen, reddish, [x+gridBox*7, y], [x+gridBox*7,y+600], 4)
-    # pygame.draw.line(screen, reddish, [x+gridBox*8, y], [x+gridBox*8,y+600], 4)
-    # pygame.draw.line(screen, reddish, [x+gridBox*9, y], [x+gridBox*9,y+600], 4)
-    #
-    # pygame.draw.line(screen, reddish, [x, y+gridBox], [x+600,y+gridBox], 4)
-    # pygame.draw.line(screen, reddish, [x, y+gridBox*2], [x+600,y+gridBox*2], 4)
-    # pygame.draw.line(screen, reddish, [x, y+gridBox*3], [x+600,y+gridBox*3], 4)
-    # pygame.draw.line(screen, reddish, [x, y+gridBox*4], [x+600,y+gridBox*4], 4)
-    # pygame.draw.line(screen, reddish, [x, y+gridBox*5], [x+600,y+gridBox*5], 4)
-    # pygame.draw.line(screen, reddish, [x, y+gridBox*6], [x+600,y+gridBox*6], 4)
-    # pygame.draw.line(screen, reddish, [x, y+gridBox*7], [x+600,y+gridBox*7], 4)
-    # pygame.draw.line(screen, reddish, [x, y+gridBox*8], [x+600,y+gridBox*8], 4)
-    # pygame.draw.line(screen, reddish, [x, y+gridBox*9], [x+600,y+gridBox*9], 4)
--- sudoku.py
+++ sudoku.py
@@ -8,39 +8,25 @@
 # for this sudoku game and that will probably lead to me making some changes
 # to the conway game.
 
-# Import libraries
 import pygame, os, sys
 from rules import *
 from vars import *
 
-# Initialize the game
-clock=pygame.time.Clock()
-
-
 pygame.init()
-
+clock=pygame.time.Clock()
 pygame.display.set_caption('Richard Castro - Sudoku Game')
-
 
 def sudoku():
     RUNTIME=Board(width, height,sqaure,66)
     RUNTIME.StartMenu()
-    # Main game loop
     while True:
         screen.fill(peach)
-
         RUNTIME.GridLines()
         clock.tick(fps)
-
-        # drawLine()
         pygame.display.update()
-        # Event Handling
         for event in pygame.event.get():
-            # Quit via close button
             if event.type == pygame.QUIT:
                 RUNTIME.QuitGame()
-
-        # drawGrid(height, width, lines, gridBox)
 
 
 if __name__ == '__main__':
